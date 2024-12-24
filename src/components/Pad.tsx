8998d922651abfd5f376549b426d9e5124b64e0f
Kamanpreet Singh <kskaman@gmail.com>
2024-12-24 18:11:16 -0330

    added keydown functionality if pad div is focused

--- src/components/Pad.tsx
+++ src/components/Pad.tsx
@@ -24,8 +24,42 @@ const Pad = ({ onUserInput }: Props) => {
     "x",
   ];
 
+  const keyToPadTag: Record<string, string> = {
+    Backspace: "DEL",
+    "+": "+",
+    "-": "-",
+    "/": "/",
+    "*": "x",
+    ".": ".",
+    "0": "0",
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8",
+    "9": "9",
+    // We'll handle Enter, =, and Escape separately
+  };
+
+  // Keydown handler on the container <div>
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const { key } = event;
+
+    if (keyToPadTag[key]) {
+      onUserInput(keyToPadTag[key]);
+    } else if (key === "Enter" || key === "=") {
+      onUserInput("=");
+    } else if (key === "Escape") {
+      onUserInput("RESET");
+    }
+  };
+
   return (
-    <div className={padStyles.pad}>
+    // We add tabIndex={0} so this div can receive focus and key events
+    <div className={padStyles.pad} tabIndex={0} onKeyDown={handleKeyDown}>
       {padButtonsTags.map((tag) => (
         <button
           key={tag}
